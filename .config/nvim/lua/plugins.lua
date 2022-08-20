9ec08fd08e1b913bc85f8b39f8505a6426b93de3
Thomas Wang <hi@xgwang.me>
2022-08-20 11:18:34 -0700

    .

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,6 +3,7 @@
 -- https://github.com/wbthomason/packer.nvim#bootstrapping
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local packer_bootstrap
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
