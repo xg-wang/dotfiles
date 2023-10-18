f8aa2bba3bffe705851d1fe377dfc3bceaa5ce96
Thomas Wang <hi@xgwang.me>
2023-10-18 10:53:55 -0700

    tabnext shortcut

--- .config/nvim/lua/vim.lua
+++ .config/nvim/lua/vim.lua
@@ -1,3 +1,6 @@
 -- Makes big files faster, probably breaks something.
 -- vim.treesitter.query.set("typescript", "injections", "")
 -- vim.treesitter.query.set("tsx", "injections", "")
+
+-- Vim shortcut for next tab when pressing leader-Tab
+vim.api.nvim_set_keymap('n', '<Leader><Tab>', ':tabnext<CR>', { noremap = true, silent = true })
