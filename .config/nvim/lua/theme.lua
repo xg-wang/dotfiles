710045380d335da18dfe663eb354decf64dc99d3
Thomas Wang <hi@xgwang.me>
2023-11-05 22:08:32 -0800

    theme

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -21,3 +21,15 @@ require("gruvbox").setup({
 })
 vim.opt.background = "dark" -- or "light" for light mode
 vim.cmd("colorscheme gruvbox")
+
+local Theme = {}
+-- shortcut F12 to toggle vim's set background between dark and light, if current is dark, use light, if current is light, use dark
+vim.api.nvim_set_keymap('n', '<F12>', ':lua require("theme").toggle_bg()<CR>', { noremap = true, silent = true })
+Theme.toggle_bg = function()
+  if vim.o.background == "dark" then
+    vim.o.background = "light"
+  else
+    vim.o.background = "dark"
+  end
+end
+return Theme
